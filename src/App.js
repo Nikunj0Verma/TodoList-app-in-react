0c2e96823f6043425a784cb7490ac8a5ba7e68ec
Nikunj <verma0025nikunj@gmail.com>
2025-08-07 17:43:40 +0530

    todolist-app-in-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") {
+      return;
+    }
+    setTodos([...todos, { text: task, completed: false}]);
+    setTask("");
+  };
+
+  const toggleComplete = (index) => {
+    const updated = [...todos];
+    updated[index].completed = !updated[index].completed;
+    setTodos(updated);
+  }
+
+
+  const deleteTask = (index) => {
+    setTodos(todos.filter((_,e) => e != index));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h1>📝 My Todo List</h1>
+        <input type="text" placeholder='What needs to be done?' value={task} onChange={(e) => setTask(e.target.value)} />
+        <button type='submit' onClick={addTask}>Add</button>
+        <div className="todo">
+          {todos.map((todo, index) => (
+            <ul>
+              <li key={index} className={todo.completed ? 'completed' : ""}>
+                <span>{todo.text}</span>
+                <div className='todo-buttons'>
+                  <i onClick={() => toggleComplete(index)} className="fa-solid fa-check"></i>
+                  {/* <i onClick={() => {handleEdit(index)}} className="fa-solid fa-pen-to-square"></i> */}
+                  <i onClick={() => { deleteTask(index) }} className="fa-solid fa-trash"></i>
+                </div>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<index/>);
+
+export default App
